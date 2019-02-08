316515859ee15494c863b34b04bed5ff38af65c8
jmojedap <jmojedap@gmail.com>
2019-02-08 17:58:33 -0500

    20% hoja de respuestas

--- application/views/respuestas/hoja_respuestas_v.php
+++ application/views/respuestas/hoja_respuestas_v.php
@@ -1,10 +1,45 @@
-<h1><?php echo $row_cuestionario->nombre_cuestionario ?></h1>
+<style>
+    @page { sheet-size: Letter; }
 
-<table class="table table-default">
-    <tbody>
-        <tr>
-            <td><?php echo $row_usuario->apellidos ?></td>
-            <td><?php echo $row_usuario->nombre ?></td>
-        </tr>
-    </tbody>
-</table>
+    table{
+        width: 70%;
+        border-collapse: collapse;
+    }
+
+    td {
+        border: 1px solid #AAA;
+        padding: 2px;
+    }
+
+    div.contenedor{
+        border: 1px solid #AAA;
+        background-image: url('<?php echo URL_IMG ?>respuestas/hoja_respuestas_700.png');
+        height: 1100px;
+        padding: 30px;
+    }
+</style>
+
+<div class="contenedor">
+    <table class="" style="">
+        <tbody>
+            <tr>
+                <td width="25%">Cuestionario</td>
+                <td>
+                    <?php echo $row_cuestionario->nombre_cuestionario ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Estudiante</td>
+                <td><?php echo $row_usuario->apellidos ?> <?php echo $row_usuario->nombre ?></td>
+            </tr>
+            <tr>
+                <td>Fecha:</td>
+                <td><?php echo date('Y-M-d') ?></td>
+            </tr>
+            <tr>
+                <td>Serial</td>
+                <td><?php echo $row_uc->id ?></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
